b25a51b4baecb67604ba3ecc2685c20b772b1f9c
mbolanosc <mrybc96@gmail.com>
2016-12-16 18:13:07 -0600

    nose

--- app/controllers/pdf.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var mongoose = require('mongoose');
-var PDFDocument = require('pdfkit');
-var blobStream  = require('blob-stream');
-var doc = new PDFDocument;
-var stream = doc.pipe(blobStream());
